d9f6dd8b8d8cd6142b39a91f8fba4dfa02e94eb7
Aleksandra Provorova <sashaprovora@gmail.com>
2025-05-13 01:22:35 +0300

    documentation and types are done

---
+++ src/types/index.ts
@@ -0,0 +1,120 @@
+// описываем возможные типы запросов на сервер и ответы на них
+export type ApiListResponse<Type> = {
+	total: number;
+	items: Type[];
+};
+
+export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+
+// описываем методы для API
+export interface ILarekAPI {
+	getProductList: () => Promise<IProduct[]>;
+	getProductItem: (id: string) => Promise<IProduct>;
+	orderProducts: (order: IOrder) => Promise<IOrderStatus>;
+}
+
+// описываем базовые типы для EventEmitter
+export type EventName = string | RegExp;
+export type Subscriber = Function;
+export type EmitterEvent = {
+	eventName: string;
+	data: unknown;
+};
+
+// описываем событийную систему
+export interface IEvents {
+	on<T extends object>(event: EventName, callback: (data: T) => void): void;
+	emit<T extends object>(event: string, data?: T): void;
+	trigger<T extends object>(
+		event: string,
+		context?: Partial<T>
+	): (data: T) => void;
+}
+
+// Данные и типы данных, используемые в приложении
+
+// описываем категории продаваемого продукта
+type ProductCategory =
+	| 'софт-скил'
+	| 'хард-скил'
+	| 'другое'
+	| 'дополнительно'
+	| 'кнопка';
+
+// описываем полученный из API продукт
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: ProductCategory;
+	price: number | null;
+}
+
+// описываем способ оплаты
+type PaymentMethod = 'cash' | 'card';
+
+// описываем форму доставки и информацию о покупателе
+export interface IOrderForm {
+	payment: PaymentMethod;
+	address: string;
+	email: string;
+	phone: string;
+}
+
+export type TDeliveryInfo = Pick<IOrderForm, 'payment' | 'address'>;
+export type TContactInfo = Pick<IOrderForm, 'email' | 'phone'>;
+
+// описываем весь заказ
+export interface IOrder extends IOrderForm {
+	items: string[];
+	total: number;
+}
+
+// описываем ответ об оформлении заказа
+export interface IOrderStatus {
+	id: string;
+	total: number;
+}
+
+// описываем ошибки валидации
+type FormErrors = Partial<Record<keyof IOrderForm, string>>;
+
+// Слой данных
+
+export interface IProductData {
+	products: IProduct[];
+	preview: string | null;
+	setProducts(items: IProduct[]): void;
+	getProduct(id: string): IProduct;
+}
+
+export interface IBasketData {
+	getProducts(): IProduct[];
+	getTotal(): number;
+	addProduct(id: IProduct): void;
+	deleteProduct(id: IProduct): void;
+	clearBasket(): void;
+}
+
+export interface IOrderDeliveryData {
+	choosePaymentType(method: PaymentMethod): void;
+	setAddress(address: string): void;
+	validateForm(data: TDeliveryInfo): boolean;
+}
+
+export interface IOrderContactsData {
+	setEmail(email: string): void;
+	setPhone(number: string): void;
+	validateForm(data: TContactInfo): boolean;
+}
+
+// Глобальные
+
+export interface IAppState {
+	products: IProduct[];
+	basket: IProduct[];
+	preview: IProduct | null;
+	order: IOrder;
+	formErrors: FormErrors;
+}
